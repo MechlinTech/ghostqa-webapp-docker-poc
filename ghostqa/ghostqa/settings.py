7a9981dc4421eca786330baccc063f772cca1d61
Diljot Singh <diljots99@gmail.com>
2024-02-22 06:14:51 +0530

    ALLOWED_HOSTS

--- ghostqa/ghostqa/settings.py
+++ ghostqa/ghostqa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-drca4hbpgs*=86g=143i$lbew#ovjd9aezzo!kctr46+odsj@#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','65.1.188.67']
 
 
 # Application definition
