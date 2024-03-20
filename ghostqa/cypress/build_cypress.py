3ad03872d1a5592b3365b9796e371827c843b0cb
Surendra-M2021 <surendra.mishra@mechlintech.com>
2024-03-21 01:59:14 +0530

    adding actions list in generate action code

--- ghostqa/cypress/build_cypress.py
+++ ghostqa/cypress/build_cypress.py
@@ -146,6 +146,20 @@ def generate_action_code(action,wrap_it=True):
     direction = action.get('direction', '')
     distance = action.get('distance', 0)
     duration = action.get('duration', 0)
+    drag_selector = action.get('drag_selector', '')
+    drop_selector = action.get('drop_selector', '')
+    form_selector = action.get('form_selector', '')
+    file_path = action.get('file_path', '')
+    x_position = action.get('x_position', 0)
+    y_position = action.get('y_position', 0)
+    url = action.get('url', '')
+    element = action.get('element', '')
+    property_name = action.get('property_name', '')
+    attribute = action.get('attribute', '')
+    expected_value = action.get('expected_value', '')
+    expected_title = action.get('expected_title', '')
+    expected_url = action.get('expected_url', '')
+    expected_length = action.get('expected_length', 0)
 
     if action_type == 'visit':
         if wrap_it:
@@ -158,7 +172,7 @@ def generate_action_code(action,wrap_it=True):
             """
         else:
             return f"""cy.visit("{selector}");""";
-    elif action_type == 'click':
+    elif action_type == 'click': # TODO confirm from QA Team and Diljot this action is left click
         if wrap_it:
             name = f'Test Step: {action_type}: {selector}'
             
@@ -270,6 +284,282 @@ def generate_action_code(action,wrap_it=True):
                 }})
             """
         return f"""cy.rightclick({selector})"""
+    elif action_type == 'clear_text':
+        if wrap_it:
+            name = f'Test Step: {action_type}: {selector}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get("{selector}").clear();
+                }});
+            """
+        return f"""cy.get("{selector}").clear();"""
+    elif action_type == 'drag_and_drop':
+        if wrap_it:
+            name = f'Test Step: {action_type}: {drag_selector} to {drop_selector}'
+            return f"""
+                it('{name}', () => {{
+                    const dataTransfer = new DataTransfer();
+                    cy.get("{drag_selector}").trigger('dragstart', {{ dataTransfer }});
+                    cy.get("{drop_selector}").trigger('drop', {{ dataTransfer }});
+                }});
+            """
+        return f"""
+            const dataTransfer = new DataTransfer();
+            cy.get("{drag_selector}").trigger('dragstart', {{ dataTransfer }});
+            cy.get("{drop_selector}").trigger('drop', {{ dataTransfer }});
+        """
+    elif action_type == 'checkbox':
+        if wrap_it:
+            name = f'Test Step: {action_type}: {selector}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get("{selector}").check();
+                }});
+            """
+        return f"""cy.get("{selector}").check();"""
+    elif action_type == 'uncheck_checkbox':
+        if wrap_it:
+            name = f'Test Step: {action_type}: {selector}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get("{selector}").uncheck();
+                }});
+            """
+        return f"""cy.get("{selector}").uncheck();"""
+    elif action_type == 'submit_form':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Submit {form_selector}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get("{form_selector}").submit();
+                }});
+            """
+        return f"""cy.get("{form_selector}").submit();"""
+    elif action_type == 'select_option':
+        if wrap_it:
+            name = f'Test step: {action_type}: Select {option} from {selector}'
+            return f"""
+                it('{name}, ()=> {{
+                    cy.get("{selector}").select("{option}");
+                }});
+            """
+        return f"""cy.get("{selector}").select("{option}");"""
+    elif action_type == 'upload_file':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Upload file {file_path} to {selector}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get("{selector}").attachFile("{file_path}");
+                }});
+            """
+        return f"""cy.get("{selector}").attachFile("{file_path}");""" 
+    elif action_type == 'scroll_into_view':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Scroll into view {selector}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get("{selector}").scrollIntoView();
+                }});
+            """
+        return f"""cy.get("{selector}").scrollIntoView();"""  
+    elif action_type == 'scroll_to_window':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Scroll window to ({x_position}, {y_position})'
+            return f"""
+                it('{name}', () => {{
+                    cy.scrollTo({x_position}, {y_position});
+                }});
+            """
+        return f"""cy.scrollTo({x_position}, {y_position});"""
+    elif action_type == 'go_to_url':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Go to URL {url}'
+            return f"""
+                it('{name}', () => {{
+                    cy.visit('{url}');
+                }});
+            """
+        return f"""cy.visit('{url}');"""
+    elif action_type == 'go_back':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Go back'
+            return f"""
+                it('{name}', () => {{
+                    cy.go('back');
+                }});
+            """
+        return f"""cy.go('back');"""
+    elif action_type == 'go_forward':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Go forward'
+            return f"""
+                it('{name}', () => {{
+                    cy.go('forward');
+                }});
+            """
+        return f"""cy.go('forward');"""
+    elif action_type == 'refresh_page':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Refresh page'
+            return f"""
+                it('{name}', () => {{
+                    cy.reload();
+                }});
+            """
+        return f"""cy.reload();"""
+    elif action_type == 'element_text_contains':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {element} text contains {text}'
+            return f"""
+                it('{name}', () => {{
+                    cy.contains('{element}', '{text}');
+                }});
+            """
+        return f"""cy.contains('{element}', '{text}');"""
+    elif action_type == 'element_exist':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} exists'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('exist');
+                }});
+            """
+        return f"""cy.get('{selector}').should('exist');"""
+    elif action_type == 'element_not_exist':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} does not exist'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('not.exist');
+                }});
+            """
+        return f"""cy.get('{selector}').should('not.exist');"""
+    elif action_type == 'element_is_visible':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} is visible'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('be.visible');
+                }});
+            """
+        return f"""cy.get('{selector}').should('be.visible');"""
+    elif action_type == 'element_is_not_visible':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} is not visible'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('not.be.visible');
+                }});
+            """
+        return f"""cy.get('{selector}').should('not.be.visible');"""
+    elif action_type == 'element_is_enabled':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} is enabled'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('be.enabled');
+                }});
+            """
+        return f"""cy.get('{selector}').should('be.enabled');"""
+    elif action_type == 'element_is_visible':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} is visible'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('be.visible');
+                }});
+            """
+        return f"""cy.get('{selector}').should('be.visible');"""
+    elif action_type == 'element_is_not_visible':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} is not visible'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('not.be.visible');
+                }});
+            """
+        return f"""cy.get('{selector}').should('not.be.visible');"""
+    elif action_type == 'element_is_enabled':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} is enabled'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('be.enabled');
+                }});
+            """
+        return f"""cy.get('{selector}').should('be.enabled');"""
+    elif action_type == 'element_is_disabled':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} is disabled'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('be.disabled');
+                }});
+            """
+        return f"""cy.get('{selector}').should('be.disabled');"""
+    elif action_type == 'element_has_value':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} has value {value}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('have.value', '{value}');
+                }});
+            """
+        return f"""cy.get('{selector}').should('have.value', '{value}');"""
+    elif action_type == 'element_has_attribute_with_value':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} has attribute {attribute} with value {value}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('have.attr', '{attribute}', '{value}');
+                }});
+            """
+        return f"""cy.get('{selector}').should('have.attr', '{attribute}', '{value}');"""
+    elif action_type == 'element_has_css_property_with_value':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} has CSS property {property_name} with value {value}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('have.css', '{property_name}', '{value}');
+                }});
+            """
+        return f"""cy.get('{selector}').should('have.css', '{property_name}', '{value}');"""
+    elif action_type == 'invoke_value':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check if element {selector} has value {expected_value}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').invoke('val').should('eq', '{expected_value}');
+                }});
+            """
+        return f"""cy.get('{selector}').invoke('val').should('eq', '{expected_value}');"""
+    elif action_type == 'validate_page_title':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Validate Page Title to include {expected_title}'
+            return f"""
+                it('{name}', () => {{
+                    cy.title().should('include', '{expected_title}');
+                }});
+            """
+        return f"""cy.title().should('include', '{expected_title}');"""
+    elif action_type == 'validate_current_url':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Validate Current URL to be {expected_url}'
+            return f"""
+                it('{name}', () => {{
+                    cy.url().should('eq', '{expected_url}');
+                }});
+            """
+        return f"""cy.url().should('eq', '{expected_url}');"""
+    elif action_type == 'number_of_element_found':
+        if wrap_it:
+            name = f'Test Step: {action_type}: Check number of elements found for selector {selector} to be {expected_length}'
+            return f"""
+                it('{name}', () => {{
+                    cy.get('{selector}').should('have.length', {expected_length});
+                }});
+            """
+        return f"""cy.get('{selector}').should('have.length', {expected_length});"""    
     elif action_type == 'assert':
         return generate_assert_code(assert_type, selector, value)
     else:
