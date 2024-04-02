4074de072a740f82e48a4954708989f792b4580a
diljots99@mechlintech.com <diljots99@mechlintech.com>
2024-04-03 03:08:28 +0530

    ps1 file

---
+++ deploy.ps1
@@ -0,0 +1,8 @@
+# Define the URL of the Docker Compose file
+$composeFileUrl = "https://raw.githubusercontent.com/MechlinTech/ghostqa-webapp-docker-poc/dockerzise/deploy.yml"
+
+# Download the Docker Compose file
+Invoke-WebRequest -Uri $composeFileUrl -OutFile "docker-compose.yml"
+
+# Run docker-compose up command
+docker compose up
