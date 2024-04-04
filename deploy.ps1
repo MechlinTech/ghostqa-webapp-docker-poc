98563d62df819ed4beb1ee3579524db73a06c0c4
diljots99@mechlintech.com <diljots99@mechlintech.com>
2024-04-04 20:16:08 +0530

    updates

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 # Define the URL of the Docker Compose file
-$composeFileUrl = "https://raw.githubusercontent.com/MechlinTech/ghostqa-webapp-docker-poc/dockerzise/deploy.yml"
+$composeFileUrl = "https://raw.githubusercontent.com/MechlinTech/ghostqa-webapp-docker-poc/main/deploy.yml"
 
 # Download the Docker Compose file
 Invoke-WebRequest -Uri $composeFileUrl -OutFile "docker-compose.yml"
