9103f35e8a25641f6e627fbe989d81bdc7a6266b
Surendra-M2021 <surendra.mishra@mechlintech.com>
2024-04-04 19:53:33 +0530

    url prefix fix

--- ghostqa/ghostqa/urls.py
+++ ghostqa/ghostqa/urls.py
@@ -34,10 +34,10 @@ router.register(r"performance-container-runs",PerformanceContainersRunsViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/',include('cypress.urls')),
-    path('api/', include(router.urls)),
-    path('api/docs/schema', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/codeengine/', include(router.urls)),
+    path('api/codeengine/docs/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('api/codeengine/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/codeengine/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 from django.conf import settings
 from django.conf.urls.static import static
